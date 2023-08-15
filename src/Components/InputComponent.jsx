c87bebfde66799fb36bdb86f2fd1e2b505d91b3a
JavierYami <javieryami1@outlook.com>
2023-08-15 17:57:08 -0600

    Props nuevas inputs

--- src/Components/InputComponent.jsx
+++ src/Components/InputComponent.jsx
@@ -1,12 +1,12 @@
 
-const InputComponent = ({placeHolder, inputType, labelValue}) => {
+const InputComponent = ({placeHolder, inputType, labelValue, onChange, name, value}) => {
 
 
 
 return(
     <div className="flex flex-col space-y-5">
         <label className="font-bold text-indigo-500">{labelValue}</label>
-        <input className="border-b border-indigo-400 bg-transparent focus:outline-none" placeholder={placeHolder} type={inputType} ></input>
+        <input className="border-b border-indigo-400 bg-transparent focus:outline-none" placeholder={placeHolder} type={inputType || 'text'} onChange={onChange} name={name} value={value} ></input>
     </div>
 )
 
--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import ButtonComponent from "./ButtonComponent";
 import InputComponent from "./InputComponent";
 
 
 const LoginForm = () => {
 
-    const handleClick = () => {
-    }
+    const [form, setForm] = useState({
+        first_name: '',
+        last_name:'',
+        email:'',
+        password:''
+    });
 
+    const handleOnChange = (event) => {
+        const {name} = event.target;
+        const {value} = event.target;
+
+        setForm({
+            ...form,
+            [name] : value,
+        })
+    }
+    
     return (
 
-        <form className="flex flex-col p-10 space-y-8 space-y-10 rounded-xl h-fit shadow-xl shadow-indigo-600/30 bg-indigo-300 w-full">
-            <InputComponent  labelValue={"First Name"}/>
-            <InputComponent labelValue={"Last name"}/>
-            <InputComponent placeHolder={"Example@example.com"} labelValue={"Email or Username"}/>
-            <InputComponent inputType={"password"} labelValue={"Password"}/>
-            <ButtonComponent buttonValue={"Login"} clickAction={handleClick} buttonType={"submit"}/>
+        <form className="flex flex-col p-10 space-y-8 space-y-10 rounded-xl h-fit shadow-xl shadow-indigo-600/30 bg-transparent w-full">
+            <InputComponent  labelValue={"First Name"} onChange={handleOnChange} name={"first_name"} value={form.first_name}/>
+            <InputComponent labelValue={"Last name"} onChange={handleOnChange} name={"last_name"} value={form.last_name}/>
+            <InputComponent placeHolder={"Example@example.com"} labelValue={"Email or Username"} onChange={handleOnChange} inputType={"email"} name={"email"} value={form.email}/>
+            <InputComponent inputType={"password"} labelValue={"Password"} onChange={handleOnChange} name={"password"} value={form.password}/>
+            <ButtonComponent buttonValue={"Login"} buttonType={"submit"}/>
         </form>
 
     )
