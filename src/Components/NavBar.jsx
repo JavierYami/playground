f78a9a2cd19435fa85b215331fda67d976a651ff
JavierYami <javieryami1@outlook.com>
2023-08-24 11:01:04 -0600

    NavBar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+
+    return (
+
+        <div className="bg-primaryBlue w-full">
+            <h1>
+                Hola
+            </h1>
+        </div>
+    )
+}
+
+export default NavBar;
